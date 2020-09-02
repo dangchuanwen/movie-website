2394a0254c698b1eceb19cb0db3147ac1e2451fd
dangchuanwen <1151517875@qq.com>
2020-09-02 20:44:45 +0800

    test:测试自动部署脚本

--- back-end/index.js
+++ back-end/index.js
@@ -6,5 +6,5 @@ const app = new Koa();
 app.use(serve(__dirname + '/dist'));
 app.use(router.routes());
 app.listen(3001, () => {
-  console.log("The serve start at port 3001.");
+  console.log("The serve start at port 3001d");
 });
