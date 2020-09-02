55fefebd6d51339b56961488511a8f00ac72bf3b
dangchuanwen <1151517875@qq.com>
2020-09-02 12:44:20 +0800

    feat: 测试github webhooks

--- back-end/model/utils/index.js
+++ back-end/model/utils/index.js
@@ -1,11 +1,11 @@
 /**
  * 二次并发查询，根据每条数据的信息进行二次查询
- * @param {Array(第一次查询获取的数组)} results 
- * @param {Function(result: 数组中的一条数据) => {}} query 
+ * @param {Array(第一次查询获取的数组)} results
+ * @param {Function(result: 数组中的一条数据) => {}} query
  */
 function mergeSeriesQuery(results, query) {
   let promises_store = [];
-  results.forEach(result => {
+  results.forEach((result) => {
     promises_store.push(query(result));
   });
   return Promise.all(promises_store);
@@ -18,25 +18,26 @@ async function queryCustomDatas(table) {
   let promises_store = mergeSeriesQuery(list, (result) => {
     // 二次查询
     const { program_id } = result;
-    const sql = `select * from list where id=${ program_id } limit 1`;
+    const sql = `select * from list where id=${program_id} limit 1`;
     return this.query(sql);
   });
 
   let promises_datas = await promises_store;
   promises_datas.forEach((result, index) => {
-    delete result[0].poster_url;
-    delete list[index].id;
-    Object.assign(list[index], result[0]);
-    // 添加 belong 和 link_url 字段
-    const item = list[index];
-    item.belong = item.table_name;
-    item.link_url = `/video?id=${item.id}`;
+    if (result && result.length > 0) {
+      delete result[0].poster_url;
+      delete list[index].id;
+      Object.assign(list[index], result[0]);
+      // 添加 belong 和 link_url 字段
+      const item = list[index];
+      item.belong = item.table_name;
+      item.link_url = `/video?id=${item.id}`;
+    }
   });
   return Promise.resolve(list);
 }
 
-
 module.exports = {
   queryCustomDatas,
-  mergeSeriesQuery
-}
+  mergeSeriesQuery,
+};
